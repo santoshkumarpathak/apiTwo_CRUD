f11c7a58efb6cb0b8f4fe00c3cc73f0ef90ead09
santoshkumarpathak <santosh96pathak@gmail.com>
2023-12-19 23:40:05 +0530

    controller added with route

---
+++ controller/contactController.js
@@ -0,0 +1,23 @@
+
+const getContacts = (req, res) => {
+    res.status(200).json({ message: "get all Contact." })
+}
+
+const getContactsById = (req, res) => {
+    res.status(200).json({ message: `Get contact for. ${req.params.id}` })
+}
+
+const postContacts = (req, res) => {
+    res.status(200).json({ message: "Create Contact." })
+}
+
+const putContacts = (req, res) => {
+    res.status(200).json({ message: `update contact for. ${req.params.id}` })
+}
+
+const deleteContacts = (req, res) => {
+    res.status(200).json({ message: `Delete contact for. ${req.params.id}` })
+}
+
+
+module.exports = {getContacts,getContactsById,postContacts,putContacts,deleteContacts}
