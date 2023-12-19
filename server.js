097ddfa4bc39a26da50564e3ee6db72a8fc03170
santoshkumarpathak <santosh96pathak@gmail.com>
2023-12-19 23:07:58 +0530

    added route folder and define the route method.

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
 const express = require('express')
+const { getContacts, getContactsById, postContacts, putContacts, deleteContacts } = require('./route/route')
 const dotenv = require('dotenv').config()
 const app = express()
-const port = process.env.PORT|| 5002
-
-app.get("/api/contacts",(req,res)=>{
- res.status(200).json({message:"Get all contact"})
-})
-
-app.get("/api/contacts/:id",(req,res)=>{
-    res.status(200).json({message:`Get contact for. ${req.params.id}`})
-})
-
-app.post("/api/contacts",(req,res)=>{
-    res.status(200).json({message:"Create successfull."})
-})
+const port = process.env.PORT || 5002
 
 
-app.put("/api/contacts/:id", (req, res) => {
-    res.status(200).json({ message: `Update for ${req.params.id}` });
-});
+app.get("/api/contacts", getContacts)
+app.get("/api/contacts/:id", getContactsById)
+app.post("/api/contacts", postContacts)
+app.put("/api/contacts/:id", putContacts)
+app.delete("/api/contacts/:id", deleteContacts)
 
-app.delete("/api/contacts/:id",(req,res)=>{
-    res.status(200).json({message:`Delete for ${req.params.id}`})
-})
+// app.get("/api/contacts",(req,res)=>{
+//  res.status(200).json({message:"Get all contact"})
+// })
+
+// app.get("/api/contacts/:id",(req,res)=>{
+//     res.status(200).json({message:`Get contact for. ${req.params.id}`})
+// })
+
+// app.post("/api/contacts",(req,res)=>{
+//     res.status(200).json({message:"Create successfull."})
+// })
 
 
-app.listen(port , ()=>{
+// app.put("/api/contacts/:id", (req, res) => {
+//     res.status(200).json({ message: `Update for ${req.params.id}` });
+// });
+
+// app.delete("/api/contacts/:id",(req,res)=>{
+//     res.status(200).json({message:`Delete for ${req.params.id}`})
+// })
+
+
+app.listen(port, () => {
     console.log(`server listing port is ${port}`)
 })
